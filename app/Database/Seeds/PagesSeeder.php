04e3ec5f960e85196a674420c7bd4353c5fa0a77
Andrew Nite <ngendesign@email.com>
2026-01-13 02:08:10 -0500

    Seeder

--- app/Database/Seeds/PagesSeeder.php
+++ app/Database/Seeds/PagesSeeder.php
@@ -3,6 +3,7 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use Faker\Factory;
 
 class PagesSeeder extends Seeder
 {
@@ -10,61 +11,57 @@ class PagesSeeder extends Seeder
     {
         $faker = Factory::create();
 
-        $notesData = [
+        $pagesInfo = [
             [
                 'id' => 1,
                 'user_id' => 1, 
-                'slug' => 'my-first-note', 
-                'title' => 'My First Notes', 
-                'priority' => 'primary', 
-                'body' => $faker->text(125),
+                'title' => 'Privacy Policy', 
+                'slug' => 'privacy-policy', 
+                'priority' => 'info', 
+                'body' => $faker->paragraphs(5),
                 'allow_comments' => false,
                 'created_at' => $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
                 'updated_at' => null,
-                'status' => 'public',
-                'type' => 1,
+                'status' => 'published',
             ],
             [
                 'id' => 2,
                 'user_id' => 1, 
-                'slug' => 'the-second-note', 
-                'title' => 'I can Fell It!', 
-                'priority' => 'success', 
-                'body' => $faker->text(125),
-                'allow_comments' => true,
-                $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
+                'title' => 'Terms of Use', 
+                'slug' => 'terms-of-use', 
+                'priority' => 'info', 
+                'body' => $faker->paragraphs(5),
+                'allow_comments' => false,
+                'created_at' => $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
                 'updated_at' => null,
-                'status' => 'private',
-                'type' => 2,
+                'status' => 'published',
             ],
             [
                 'id' => 3,
                 'user_id' => 2, 
                 'slug' => 'the-best-title', 
                 'title' => 'Something New Note', 
-                'priority' => 'success', 
-                'body' => $faker->text(125),
+                'priority' => 'general', 
+                'body' => $faker->paragraphs(5),
                 'allow_comments' => true,
-                $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
+                'created_at' => $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
                 'updated_at' => null,
-                'status' => 'archived',
-                'type' => 3,
+                'status' => 'published',
             ],
             [
                 'id' => 4,
                 'user_id' => 2, 
                 'slug' => 'what-now-im-woundering', 
                 'title' => 'What is going on?', 
-                'priority' => 'success', 
-                'body' => $faker->text(125),
+                'priority' => 'general', 
+                'body' => $faker->paragraphs(5),
                 'allow_comments' => true,
-                $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
+                'created_at' => $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
                 'updated_at' => null,
-                'status' => 'public',
-                'type' => 4
+                'status' => 'published',
             ],
         ];
 
-        $this->db->table('notes')->insertBatch($notesData);
+        $this->db->table('pages')->insertBatch($pagesInfo);
     }
 }
