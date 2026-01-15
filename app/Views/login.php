b0b1c31fb71f4e7c2f256fb3cb5b2d438f83f224
Andrew Nite <ngendesign@email.com>
2026-01-14 22:10:38 -0500

    Login pages

---
+++ app/Views/login.php
@@ -0,0 +1,123 @@
+<?= $this->extend('layouts/main'); ?>
+
+<?= $this->section('main'); ?>
+
+<div class="wrapper">
+    <section class="login-content">
+        <div class="container h-100">
+            <div class="row justify-content-center align-items-center height-self-center">
+
+                <div class="col-md-5 col-sm-12 col-12 align-self-center">
+                    <!-- ALERTS -->
+                    <div class="mt-5">
+                        <?php if (session('error') !== null): ?>
+                            <div class="alert bg-white alert-danger" role="alert">
+                                <div class="iq-alert-icon">
+                                    <i class="bi bi-exclamation-circle"></i>
+                                </div>
+                                <div class="iq-alert-text"><?= esc(session('error')) ?></div>
+                            </div>
+                        <?php elseif (session('errors') !== null): ?>
+                            <div class="alert bg-white alert-danger" role="alert">
+                                <div class="iq-alert-icon">
+                                    <i class="bi bi-exclamation-circle"></i>
+                                </div>
+                                <div class="iq-alert-text">
+                                    <?php if (is_array(session('errors'))): ?>
+                                        <?php foreach (session('errors') as $error): ?>
+                                            <?= esc($error) ?>
+                                            <br>
+                                        <?php endforeach ?>
+                                    <?php else: ?>
+                                        <?= esc(session('errors')) ?>
+                                    <?php endif ?>
+                                </div>
+                            </div>
+                        <?php endif ?>
+
+                        <?php if (session('message') !== null): ?>
+                            <div class="alert bg-white alert-success" role="alert">
+                                <div class="iq-alert-icon">
+                                    <i class="bi bi-exclamation-square-fill"></i>
+                                </div>
+                                <div class="iq-alert-text"><?= esc(session('message')) ?></div>
+                                <?= esc(session('message')) ?>
+                            </div>
+                        <?php endif ?>
+                    </div>
+                    <!-- SignIn User Card -->
+                    <div class="sign-user_card">
+                        <div class="logo-detail">
+                            <div class="d-flex align-items-center"><img src="assets/images/logo.png"
+                                    class="img-fluid rounded-normal light-logo logo" alt="logo">
+                                <h4 class="logo-title ml-3"><?= esc($appName) ?></h4>
+                            </div>
+                        </div>
+                        <h3 class="mb-2"><?= lang('Auth.login') ?></h3>
+                        <p><?= lang('Auth.headline') ?></p>
+                        <form action="<?= url_to('login') ?>" method="post">
+                            <?= csrf_field() ?>
+
+                            <div class="row">
+
+                                <!-- Email -->
+                                <div class="col-lg-12">
+                                    <div class="floating-label form-group">
+                                        <input class="floating-input form-control" type="email" name="email"
+                                            inputmode="email" autocomplete="email"
+                                            placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>"
+                                            required>
+                                        <label><?= lang('Auth.email') ?></label>
+                                    </div>
+                                </div>
+
+                                <!-- Password -->
+                                <div class="col-lg-12">
+                                    <div class="floating-label form-group">
+                                        <input class="floating-input form-control" type="password" name="password"
+                                            inputmode="text" autocomplete="current-password"
+                                            placeholder="<?= lang('Auth.password') ?>" required>
+                                        <label><?= lang('Auth.password') ?></label>
+                                    </div>
+                                </div>
+                                
+                                <!-- Remember me -->
+                                <?php if (setting('Auth.sessionConfig')['allowRemembering']): ?>
+                                    <div class="col-lg-6">
+                                        <div class="custom-control custom-checkbox mb-3 text-left">
+                                            <input type="checkbox" name="remember" class="custom-control-input" id="rememberMeKumbuka" <?php if (old('remember')): ?> checked<?php endif ?>>
+                                            <label class="custom-control-label" for="rememberMeKumbuka"><?= lang('Auth.rememberMe') ?></label>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+
+                                <!-- MagicLink -->
+                                <?php if (setting('Auth.allowMagicLinkLogins')): ?>
+                                    <div class="col-lg-6">
+                                        <a href="<?= url_to('magic-link') ?>"
+                                            class="text-primary float-right"><?= lang('Auth.forgotPassword') ?></a>
+                                    </div>
+                                <?php endif ?>
+                            </div>
+
+                            <!-- Sign In Button -->
+                            <button type="submit" class="btn btn-primary"><?= lang('Auth.loginButton') ?></button>
+                            
+                            <!-- Registration Link -->
+                            <?php if (setting('Auth.allowRegistration')): ?>
+                                <p class="mt-3 mb-0">
+                                    <?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>"
+                                        class="text-primary"><b><?= lang('Auth.register') ?></b></a>
+                                </p>
+                            <?php endif ?>
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<!-- END Main -->
+<?= $this->endSection(); ?>
