db2ab913db641526fea4f1c9f2e309aaaac0b68a
Andrew Nite <ngendesign@email.com>
2026-01-23 19:49:15 -0500

    Included new routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,9 +3,14 @@
 use App\Controllers\Home;
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\CustomErrors;
+// Admin Controllers
+use App\Controllers\Admin;
+// General User Pages
 use App\Controllers\Dashboard;
 use App\Controllers\Notes;
 use App\Controllers\Pages;
+
+
 
 /**
  * @var RouteCollection $routes
@@ -24,7 +29,7 @@ service('auth')->routes($routes);
 $routes->group('/', function ($routes) {
     $routes->get('', [Home::class, 'index']);
     $routes->get('privacy_policy', [Pages::class, 'privacy_policy']);
-    $routes->get('terms_and_conditions', [Pages::class, 'terms_and_conditions']);
+    $routes->get('terms_of_use', [Pages::class, 'terms_of_use']);
     $routes->get('support', [Pages::class, 'support']);
     // Sucessful Logout
     $routes->get('logged_out', [Pages::class, 'logged_out']);
@@ -55,10 +60,20 @@ $routes->group('',['filter' => ['userfilter']], function ($routes) {
  */
 $routes->group('admin',['filter' => ['adminfilter']], function ($routes) {
 
-    // User Account Routes
-    $routes->group('account', static function ($routes) {
-        
-    });
+    $routes->get('dashboard', [Admin\Dashboard::class, 'index']);
+ 
+    // User Management
+    $routes->get('users', [Admin\Users::class, 'index']);
+    $routes->get('users/create', [Admin\Users::class, 'create']);
+    $routes->post('users/store', [Admin\Users::class, 'store']);
+    $routes->get('users/edit/(:num)', [[Admin\Users::class, 'edit'], '$1']);
+    $routes->post('users/update/(:num)', [[Admin\Users::class, 'update'], '$1']);
+    $routes->get('users/delete/(:num)', [[Admin\Users::class, 'delete'],'$1']);
+    
+    // Settings
+    $routes->get('settings', [Admin\Settings::class, 'index']);
+    $routes->post('settings/update', [Admin\Settings::class, 'update']);
+
 
 
 });
