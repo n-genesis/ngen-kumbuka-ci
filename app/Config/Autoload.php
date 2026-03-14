3d54916c394ce1b8a95694dea360412b9faac705
Andrew Nite <ngendesign@email.com>
2026-03-13 22:38:14 -0400

    Created ne helper function

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -88,5 +88,5 @@ class Autoload extends AutoloadConfig
      *
      * @var list<string>
      */
-    public $helpers = ['auth', 'setting', 'form','activity'];
+    public $helpers = ['auth', 'setting', 'form','activity','user_settings'];
 }
