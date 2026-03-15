88e2db27a4431113939ca25b248b6761a509c6e2
Andrew Nite <ngendesign@email.com>
2026-03-15 02:59:13 -0400

    Updated meigration script

--- app/Database/Migrations/2026-01-13-071821_CreateFollowersTable.php
+++ app/Database/Migrations/2026-01-13-071821_CreateFollowersTable.php
@@ -15,9 +15,10 @@ class CreateFollowersTable extends Migration
                 'type'       => 'ENUM',
                 'constraint' => ['pending','accepted','blocked'], // Define the ENUM values here
                 'default'    => 'pending',
-                'null'       => false,
+                'null'       => true,
             ],
             'created_at' => ['type' => 'DATETIME', 'null' => true],
+            'updated_at' => ['type' => 'DATETIME', 'null' => true],
             'deleted_at' => ['type' => 'DATETIME', 'null' => true],
         ]);
         $this->forge->addPrimaryKey(['follower_id', 'followed_id']);
